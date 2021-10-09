c7b73141b9e8fe84ff3f70ce424ba4d5f0962a03
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-09 15:18:56 +0100

    render navbar links if user is signed in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,21 @@
 
-const Navbar = () => {
+const Navbar = ({ userAuthorised, setUserAuthorised }) => {
     return (
         <nav>
+            <h1>k'nect</h1>
 
+            {userAuthorised &&
+                <div className="nav-actions">
+                    <Link to={`/home`}>
+                        Home
+                    </Link>
+                    <Link to={`/profile`}>
+                        Profile
+                    </Link>
+                    <button onClick={onSignOut}>
+                        Sign Out
+                    </button>
+                </div>}
         </nav>
     )
 }

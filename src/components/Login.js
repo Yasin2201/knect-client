da5265b12aebc0785eecc2ca0517a88fe6d1bf88
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-09 15:11:36 +0100

    add Login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,8 +4,20 @@ const Login = ({ setUserAuthorised }) => {
 
 
     return (
-        <div>
-
+        <div className="login-div">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={onLogin}>
+                <label htmlFor="username">Username:</label>
+                <input id="username" name="username" type="text" />
+                <label htmlFor="password">Password:</label>
+                <input id="password" name="password" type="password" />
+                <button type="submit">Sign In</button>
+            </form>
+            {message &&
+                <p className="error-message">
+                    {message}!
+                </p>
+            }
         </div>
     )
 }

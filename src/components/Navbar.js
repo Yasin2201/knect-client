c969a99b3cc7b5c8a6cfe3b9a769df1ead752e2b
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-09 15:20:39 +0100

    removeToken and set userAuth to false when user signs out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom"
 
 const Navbar = ({ userAuthorised, setUserAuthorised }) => {
+
+    const onSignOut = async (e) => {
+        e.preventDefault()
+
+        try {
+            await fetch(`http://localhost:3000/sign-out`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            })
+            sessionStorage.removeItem('token')
+            sessionStorage.removeItem('userAuth')
+            setUserAuthorised(false)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     return (
         <nav>
             <h1>k'nect</h1>

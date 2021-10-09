375c9cd7c98666c1f1f5d20ecc810d4363f92e4e
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-09 17:02:51 +0100

    add newuser to database when signed up successfully else display error message

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,42 @@
+import { useState } from "react";
+
 const Signup = () => {
+    const [message, setMessage] = useState()
+
+    const onSignUp = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const userInput = {
+            username: formData.get('username'),
+            password: formData.get('password')
+        }
+
+        try {
+            const response = await fetch(`http://localhost:3000/sign-up`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userInput)
+            })
+            const data = await response.json()
+
+            if (!data.user) {
+                setMessage(data.alerts[0].msg)
+            } else {
+                setMessage()
+            }
+        } catch (err) {
+            console.error(err)
+        }
+        e.target.reset()
+    }
+
     return (
         <div className="signup-div">
             <h2>Sign Up</h2>
-            <form className="signup-form" onSubmit={onLogin}>
+            <form className="signup-form" onSubmit={onSignUp}>
                 <label htmlFor="username">Username:</label>
                 <input id="username" name="username" type="text" />
                 <label htmlFor="password">Password:</label>

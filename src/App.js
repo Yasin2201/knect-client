54b1cbe8b144ccee2fec803198b2a0990302259e
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-09 15:15:24 +0100

    use React Router to render Login form or Logged in page if user logs in succesfully

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import Login from './components/Login';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom"
+import { useEffect, useState } from 'react';
+
 const App = () => {
+  const [userAuthorised, setUserAuthorised] = useState(false)
+
+  useEffect(() => {
+    const userAuth = sessionStorage.getItem('userAuth')
+    if (userAuth) {
+      setUserAuthorised(true)
+    } else {
+      setUserAuthorised(false)
+    }
+  }, [])
+
   return (
     <div>
-      Knect App
+      <BrowserRouter>
+
+        {!userAuthorised ?
+          <Switch>
+            <Route exact path='/login'>
+              <Login setUserAuthorised={setUserAuthorised} />
+            </Route>
+          </Switch>
+          :
+          <div>
+            LOGGED IN!
+          </div>
+        }
+
+      </BrowserRouter>
     </div>
   );
 }

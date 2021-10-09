4fc9464614a501678a733ba7ba556fb2c04bbdc3
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-09 16:58:14 +0100

    added signup form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,19 @@
 const Signup = () => {
     return (
-        <div>
-
+        <div className="signup-div">
+            <h2>Sign Up</h2>
+            <form className="signup-form" onSubmit={onLogin}>
+                <label htmlFor="username">Username:</label>
+                <input id="username" name="username" type="text" />
+                <label htmlFor="password">Password:</label>
+                <input id="password" name="password" type="password" />
+                <button type="submit">Sign In</button>
+            </form>
+            {message &&
+                <p className="error-message">
+                    {message}!
+                </p>
+            }
         </div>
     )
 }

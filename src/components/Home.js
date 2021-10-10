90d653a574a495de793d87e431dc830616873dd4
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-10 18:30:08 +0100

    get all current users and current users friends posts and save in postsInfo state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+
 const Home = ({ currUser }) => {
+    const [postsInfo, setPostsInfo] = useState([])
+
+    useEffect(() => {
+        if (currUser) {
+            const getAllPosts = async () => {
+                try {
+                    const res = await fetch(`http://localhost:3000/${currUser}/timeline`, {
+                        method: 'GET',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                        },
+                    })
+                    const data = await res.json()
+                    setPostsInfo(data)
+                } catch (err) {
+                    throw err
+                }
+            }
+            getAllPosts()
+        }
+    }, [currUser])
 
     return (
-        <div></div>
+        <div>
+
+        </div>
+
     )
 };
 

8d383839da26bc5ad59526ef3bb9bacfbd7f4d77
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-11 18:07:27 +0100

    fecth posts comments data from API

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,32 @@
-const Comment = () => {
+import { useEffect } from "react";
+
+const Comment = ({ postID }) => {
+
+    useEffect(() => {
+
+        try {
+            const getComments = async () => {
+                const res = await fetch(`http://localhost:3000/${postID}/comments`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                    },
+                });
+
+                const data = await res.json()
+
+
+
+            }
+
+            getComments()
+        } catch (error) {
+            throw error
+        }
+
+    }, [postID])
 
     return (
         <div></div>

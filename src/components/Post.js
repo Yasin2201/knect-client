5eed984b07448948ee7700e2b9f98223899fdb6e
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-11 18:15:45 +0100

    render comments under its post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,13 @@
+import Comment from './Comment'
+
 const Post = ({ postData }) => {
     return (
-        <div>
+        <div style={{ border: '2px solid black' }}>
             <p>{postData.postUsername}</p>
             <p>{postData.text}</p>
             <p>Likes: {postData.likes.length}</p>
             <p>{postData.date} @ {postData.time}</p>
+            <Comment postID={postData._id} />
         </div>
     )
 }

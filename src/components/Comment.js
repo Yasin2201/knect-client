542bcac37247ee7817bb32c4aad8310cab2bf678
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-12 17:30:33 +0100

    format date and time on each comment in comments array and render information

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,60 +1,48 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-const Comment = ({ postID }) => {
-    const [commentsInfo, setCommentsInfo] = useState([])
+const Comment = ({ comments }) => {
+    const [formattedComments, setFormattedComments] = useState([])
 
     useEffect(() => {
-        try {
-            const getComments = async () => {
-                const res = await fetch(`http://localhost:3000/${postID}/comments`, {
-                    method: 'GET',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
-                    },
-                });
+        const formattedData = comments.map((comment) => {
+            const formattedDate = new Date(comment.date).toLocaleDateString("en-gb", {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+            })
+            const formattedTime = new Date(comment.date).toLocaleTimeString("en-US", {
+                hour: "numeric",
+                minute: "numeric",
+            })
 
-                const data = await res.json()
-
-                const formattedData = data.comments.map((comment) => {
-                    const commentUsername = data.allUsers.find(user => comment.userId === user._id).username
-                    const formattedDate = new Date(comment.date).toLocaleDateString("en-gb", {
-                        year: "numeric",
-                        month: "short",
-                        day: "numeric",
-                    })
-                    const formattedTime = new Date(comment.date).toLocaleTimeString("en-US", {
-                        hour: "numeric",
-                        minute: "numeric",
-                    })
-                    return {
-                        commentUsername,
-                        ...comment,
-                        date: formattedDate,
-                        time: formattedTime
-                    }
-                })
-                setCommentsInfo(formattedData)
+            return {
+                ...comment,
+                date: formattedDate,
+                time: formattedTime
             }
-            getComments()
-        } catch (error) {
-            throw error
-        }
-
-    }, [postID])
-
-    return (
-        commentsInfo.length > 0 && commentsInfo.map((comment) => {
-            return (
-                <div key={comment._id} style={{ border: '1px solid red' }}>
-                    <p>{comment.commentUsername}</p>
-                    <p>{comment.text}</p>
-                    <p>Likes: {comment.likes.length}</p>
-                    <p>{comment.date} @ {comment.time}</p>
-                </div>
-            )
         })
+        setFormattedComments(formattedData)
+    }, [comments])
+
+    // console.log(formattedComments)
+    return (
+        <div>
+            {
+                formattedComments.length > 0 &&
+                formattedComments.map((comment) => {
+                    return (
+                        <div key={comment._id} style={{ border: '1px solid red' }}>
+                            <Link to={`/profile/${comment.userId}`}>{comment.commentUsername}</Link>
+                            <p>{comment.text}</p>
+                            <p>Likes: {comment.likes.length}</p>
+                            <p>{comment.date} @ {comment.time}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+
     )
 }
 

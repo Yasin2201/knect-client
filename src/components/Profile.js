aefe898b2dbd7c0b2b7e82843fb1ec46871b08a3
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-12 14:04:59 +0100

    render GET users details from api

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+//TO-DO----
+//Get current users details
+//Get friends list same as above res
+//Get users posts
+//Get liked posts
+
 const Profile = () => {
+    const [userDetails, setUserDetails] = useState()
+    const { id } = useParams();
 
+    useEffect(() => {
+        async function getUsersDetails() {
+            try {
+                const response = await fetch(`http://localhost:3000/profile/${id}`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                    },
+                })
+                const data = await response.json()
 
+                if (response.status === 200) {
+                    setUserDetails(data)
+                }
+            } catch (err) {
+                throw err
+            }
+        }
+
+        getUsersDetails()
+
+    }, [id])
+
+    console.log(userDetails)
     return (
         <div>
 

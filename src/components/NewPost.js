97837240153b44260a4cebae696c535a6072be3a
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-13 15:23:31 +0100

    render POST new post to database

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,14 +1,53 @@
-const NewPost = ({ postsInfo, setPostsInfo }) => {
+import { useState } from "react";
+
+const NewPost = ({ currUser }) => {
+    const [errors, setErrors] = useState();
 
     const submitNewPost = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const userInput = {
+            text: formData.get('text'),
+        }
 
+        try {
+            const response = await fetch(`http://localhost:3000/${currUser}/new-post`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                },
+                body: JSON.stringify(userInput)
+            })
+            const data = await response.json()
+
+            if (data.errors) {
+                setErrors(data.alerts)
+            } else {
+                setErrors()
+                window.location.reload(false);
+            }
+        } catch (err) {
+            throw err
+        }
     }
 
     return (
-        <form className="new-comment-form" onSubmit={(e) => submitNewPost(e)}>
-            <textarea type="text" name="text" placeholder="Share your thoughts..." />
-            <button className="submit-btn" type="submit">Submit</button>
-        </form>
+        <div>
+            <form className="new-post-form" onSubmit={(e) => submitNewPost(e)}>
+                <textarea type="text" name="text" placeholder="Share your thoughts..." />
+                <button className="submit-btn" type="submit">Submit</button>
+            </form>
+
+            {errors && errors.map((error) => {
+                return (
+                    <div key={errors.indexOf(error)} className="error-message">
+                        {error.msg}!
+                    </div>
+                )
+            })}
+        </div>
     )
 };
 

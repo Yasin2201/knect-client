6b15b12c7e6f46512716d1d7eb89f64a1768d615
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-14 12:12:37 +0100

    add functionality to change post text when editing

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,16 +4,24 @@ import Comment from './Comment';
 
 const Post = ({ data }) => {
     const [isEditing, setIsEditing] = useState(false)
+    const [textData, setTextData] = useState('')
 
     const editPost = () => {
         setIsEditing(!isEditing)
     }
 
+    const editTextData = (e) => {
+        setTextData(e.target.value)
+    }
+
     return (
         isEditing
             ?
-            <div>
-                <p>Editing</p>
+            <div style={{ border: '2px solid black' }}>
+                <p>{data.username}</p>
+                <textarea type="text" name="text" onChange={editTextData} defaultValue={data.text} />
+                <p>Likes: {data.likes.length}</p>
+                <p>{data.date} @ {data.time}</p>
                 <button onClick={editPost}>Cancel</button>
             </div>
             :

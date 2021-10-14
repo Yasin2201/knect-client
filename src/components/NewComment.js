a57798d50c3d22e1a004a880d90db227a5288bba
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-14 19:50:54 +0100

    render form and any errors returned in component

--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -1,12 +1,24 @@
 import { useState } from "react";
 
-const NewComment = () => {
+const NewComment = ({ currUser, postId, setComments, comments }) => {
+    const [errors, setErrors] = useState();
 
 
 
     return (
         <div>
+            <form className="new-comment-form" onSubmit={(e) => submitNewComment(e)}>
+                <textarea type="text" name="text" placeholder="Leave a comment..." />
+                <button className="submit-btn" type="submit">Submit</button>
+            </form>
 
+            {errors && errors.map((error) => {
+                return (
+                    <div key={errors.indexOf(error)} className="error-message">
+                        {error.msg}!
+                    </div>
+                )
+            })}
         </div>
     )
 };

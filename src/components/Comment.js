41107012ea54b2550ab6e629265c4c6bf1e0c480
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-14 18:56:20 +0100

    allow users to toggleEdit of their comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Comment = ({ data }) => {
+    const [isEditing, setIsEditing] = useState(false)
+    const currUser = sessionStorage.getItem('currUser')
+
+    const toggleEdit = () => {
+        setIsEditing(!isEditing)
+    }
 
     return (
-        <div>
+        isEditing ?
+            <div key={data._id} style={{ border: '1px solid red' }}>
+                <p>{data.username}</p>
+                <textarea type="text" name="text" defaultValue={data.text} />
+                <p>Likes: {data.likes.length}</p>
+                <p>{data.date} @ {data.time}</p>
+                {currUser === data.userId && <button onClick={toggleEdit}>Cancel</button>}
+            </div>
+            :
             <div key={data._id} style={{ border: '1px solid red' }}>
                 <Link to={`/profile/${data.userId}`}>{data.username}</Link>
                 <p>{data.text}</p>
                 <p>Likes: {data.likes.length}</p>
                 <p>{data.date} @ {data.time}</p>
+                {currUser === data.userId && <button onClick={toggleEdit}>Edit</button>}
             </div>
-        </div>
-
     )
 }
 

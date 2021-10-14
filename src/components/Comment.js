9eddbf7898f0b0487887ea64b74638f1fc00d1df
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-14 19:17:14 +0100

    let users like and unlike comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -66,6 +66,30 @@ const Comment = ({ data, comments, setComments }) => {
         }
     }
 
+    const toggleLike = async () => {
+        try {
+            const res = await fetch(`http://localhost:3000/${currUser}/like-comment/${data._id}`, {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                },
+            })
+            const responseData = await res.json()
+
+            const updatedComments = comments.map((comment) => {
+                return comment._id === data._id ? { ...comment, likes: responseData.newComment.likes } : comment
+            })
+
+            if (res.status === 201) {
+                setComments(updatedComments)
+            }
+        } catch (err) {
+            throw err
+        }
+    }
+
     return (
         isEditing ?
             <div key={data._id} style={{ border: '1px solid red' }}>
@@ -82,6 +106,7 @@ const Comment = ({ data, comments, setComments }) => {
                 <p>{data.text}</p>
                 <p>Likes: {data.likes.length}</p>
                 <p>{data.date} @ {data.time}</p>
+                {data.likes.includes(currUser) ? <button onClick={toggleLike}>Unlike</button> : <button onClick={toggleLike}>Like</button>}
                 {currUser === data.userId && <button onClick={deleteComment}>Delete</button>}
                 {currUser === data.userId && <button onClick={toggleEdit}>Edit</button>}
             </div>

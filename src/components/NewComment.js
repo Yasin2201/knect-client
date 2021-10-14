b11000840283f6d8df4b40fb9ca78c0ada108112
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-14 19:51:26 +0100

    POST new comment to database and render under post

--- src/components/NewComment.js
+++ src/components/NewComment.js
@@ -3,7 +3,53 @@ import { useState } from "react";
 const NewComment = ({ currUser, postId, setComments, comments }) => {
     const [errors, setErrors] = useState();
 
+    const submitNewComment = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const userInput = {
+            text: formData.get('text'),
+        }
 
+        try {
+            const response = await fetch(`http://localhost:3000/${currUser}/post/${postId}/new-comment`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                },
+                body: JSON.stringify(userInput)
+            })
+            const data = await response.json()
+
+            if (data.errors) {
+                setErrors(data.alerts)
+            } else {
+                setErrors()
+                e.target.reset()
+
+                const formattedDate = new Date(data.comment.date).toLocaleDateString("en-gb", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                })
+                const formattedTime = new Date(data.comment.date).toLocaleTimeString("en-US", {
+                    hour: "numeric",
+                    minute: "numeric",
+                })
+
+                const formattedNewComment = {
+                    ...data.comment,
+                    date: formattedDate,
+                    time: formattedTime
+                }
+
+                setComments([formattedNewComment, ...comments])
+            }
+        } catch (err) {
+            throw err
+        }
+    }
 
     return (
         <div>

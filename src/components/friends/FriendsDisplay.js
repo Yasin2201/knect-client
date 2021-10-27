ef86ee242aaf44541fa11d5d3da9b9597ff3c2d2
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-27 14:31:33 +0100

    Render FriendsList, ReceivedFriendRequests & SentFriendRequests components in FriendsDisplay Component

--- src/components/friends/FriendsDisplay.js
+++ src/components/friends/FriendsDisplay.js
@@ -1,7 +1,13 @@
+import FriendsList from "./FriendsList";
+import ReceivedFriendRequests from "./ReceivedFriendRequests";
+import SentFriendRequests from "./SentFriendRequests";
+
 const FriendsDisplay = () => {
     return (
         <div>
-
+            <FriendsList />
+            <ReceivedFriendRequests />
+            <SentFriendRequests />
         </div>
     )
 }
--- src/components/friends/ReceivedFriendRequest.js
+++
@@ -1,9 +0,0 @@
-const ReceivedFriendRequests = () => {
-    return (
-        <div>
-            Received Friend Requests
-        </div>
-    )
-}
-
-export default ReceivedFriendRequests;

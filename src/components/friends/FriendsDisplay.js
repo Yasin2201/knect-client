671032ee794e35077c1daf5c0fc98a0b860cb9ad
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-27 14:48:21 +0100

    Conditionally render which component to display depending on the button user has selected

--- src/components/friends/FriendsDisplay.js
+++ src/components/friends/FriendsDisplay.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import FriendsList from "./FriendsList";
 import ReceivedFriendRequests from "./ReceivedFriendRequests";
 import SentFriendRequests from "./SentFriendRequests";
 
 const FriendsDisplay = () => {
+    let [viewOption, setViewOption] = useState('All Friends')
+    let currentView;
+
+    const ToggleDisplayedComponent = (e) => {
+        const clickedComponent = e.target.textContent
+        setViewOption(clickedComponent)
+    }
+
+    if (viewOption === 'All Friends') {
+        currentView = <FriendsList />
+    } else if (viewOption === 'Received Requests') {
+        currentView = <ReceivedFriendRequests />
+    } else if (viewOption === 'Sent Requests') {
+        currentView = <SentFriendRequests />
+    }
+
     return (
         <div>
-            <FriendsList />
-            <ReceivedFriendRequests />
-            <SentFriendRequests />
+            <button onClick={ToggleDisplayedComponent}>All Friends</button>
+            <button onClick={ToggleDisplayedComponent}>Received Requests</button>
+            <button onClick={ToggleDisplayedComponent}>Sent Requests</button>
+            {currentView}
         </div>
     )
 }

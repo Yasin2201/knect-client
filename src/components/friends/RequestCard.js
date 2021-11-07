07a1b9719efc1a02db8aa9f5fa6a8df29342f93d
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-07 16:32:35 +0000

    delete friend requests when user declines request

--- src/components/friends/RequestCard.js
+++ src/components/friends/RequestCard.js
@@ -25,12 +25,30 @@ const RequestCard = ({ data, requests, setRequests }) => {
         }
     }
 
+    const declineRequest = async () => {
+        try {
+            const res = await fetch(`http://localhost:3000/${data.recipient}/request/${data._id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                },
+            })
+            const resData = await res.json()
+            console.log(resData)
+            filterFriendReqs()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div>
             <div>
                 <Link to={`/profile/${data.requester._id}`}>{data.requester.username}</Link>
                 <button onClick={acceptRequest}>Accept</button>
-                <button>Decline</button>
+                <button onClick={declineRequest}>Decline</button>
             </div>
         </div>
     )

dc923188e1e783907b198a7fd349e65aae60a582
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-07 16:26:56 +0000

    add RequestCard and update users friends and friend requests when user accepts request

---
+++ src/components/friends/RequestCard.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+const RequestCard = ({ data, requests, setRequests }) => {
+
+    const filterFriendReqs = () => {
+        const filteredReqs = requests.filter((req) => req._id !== data._id)
+        setRequests(filteredReqs)
+    }
+
+    const acceptRequest = async () => {
+        try {
+            const res = await fetch(`http://localhost:3000/${data.recipient}/request/${data._id}`, {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                },
+            })
+            const resData = await res.json()
+            console.log(resData)
+            filterFriendReqs()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <Link to={`/profile/${data.requester._id}`}>{data.requester.username}</Link>
+                <button onClick={acceptRequest}>Accept</button>
+                <button>Decline</button>
+            </div>
+        </div>
+    )
+}
+
+export default RequestCard;

856d0746e48018310698922815a4660448c0c80d
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-07 15:26:53 +0000

    get all recieved friend requests for user from api

--- src/components/friends/ReceivedFriendRequests.js
+++ src/components/friends/ReceivedFriendRequests.js
@@ -1,4 +1,30 @@
-const ReceivedFriendRequests = () => {
+import { useEffect } from "react"
+
+const ReceivedFriendRequests = ({ currUser }) => {
+
+
+    useEffect(() => {
+        try {
+            async function getReceivedReqs() {
+                const res = await fetch(`http://localhost:3000/${currUser}/recieved-requests`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                    },
+                })
+                const data = await res.json()
+                console.log(data.all_requests)
+
+            }
+            getReceivedReqs()
+
+        } catch (error) {
+            console.log(error)
+        }
+    }, [currUser])
+
     return (
         <div>
             Received Friend Requests

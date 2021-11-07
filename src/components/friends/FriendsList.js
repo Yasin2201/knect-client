41f11ae28b5c21c2796ce522e02434b6e7f05d51
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-07 14:58:29 +0000

    get all users friends list data from api

--- src/components/friends/FriendsList.js
+++ src/components/friends/FriendsList.js
@@ -1,4 +1,28 @@
-const FriendsList = () => {
+import { useEffect } from "react";
+
+const FriendsList = ({ currUser }) => {
+    console.log(currUser)
+
+    useEffect(() => {
+        try {
+            async function getFriends() {
+                const res = await fetch(`http://localhost:3000/${currUser}/friends`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                })
+                const data = await res.json()
+                console.log(data)
+
+            }
+            getFriends()
+        } catch (error) {
+            console.log(error)
+        }
+    }, [currUser])
+
     return (
         <div>
             Friends List

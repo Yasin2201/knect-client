3b9158c38fc206a364ef5b561124f412b4ebf3da
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-11-10 15:49:28 +0000

    get all users sent requests from backend and store in request state

--- src/components/friends/SentFriendRequests.js
+++ src/components/friends/SentFriendRequests.js
@@ -1,4 +1,32 @@
-const SentFriendRequests = () => {
+import { useEffect, useState } from "react"
+
+const SentFriendRequests = ({ currUser }) => {
+    const [requests, setRequests] = useState([])
+
+    useEffect(() => {
+        try {
+            async function getSentReqs() {
+                const res = await fetch(`http://localhost:3000/${currUser}/sent-requests`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                    },
+                })
+                const data = await res.json()
+
+                if (data.all_requests) {
+                    setRequests(data.all_requests)
+                }
+            }
+            getSentReqs()
+
+        } catch (error) {
+            console.log(error)
+        }
+    }, [currUser])
+
     return (
         <div>
             Sent Friend Requests
